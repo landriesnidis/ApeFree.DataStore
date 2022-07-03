5701c5d0b2de0beea4a12d59f0c3e6a888244e16
Guijie Lee <landriesnidis@yeah.net>
2022-07-04 00:08:32 +0800

    注册表存储器（仅适用于Windows平台）

---
+++ ApeFree.DataStore.Registry/RegistryStore.cs
@@ -0,0 +1,88 @@
+﻿using ApeFree.DataStore.Core;
+using Microsoft.Win32;
+using System;
+using System.IO;
+
+namespace ApeFree.DataStore.Registry
+{
+    /// <summary>
+    /// 注册表(对象)储存器
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class RegistryStore<T> : Store<T, RegistryStoreAccessSettings> where T : new()
+    {
+        private static readonly RegistryView registryView
+            = Environment.Is64BitOperatingSystem ? RegistryView.Registry64 : RegistryView.Registry32;
+
+        /// <summary>
+        /// 构造注册表(对象)储存器
+        /// </summary>
+        /// <param name="settings"></param>
+        public RegistryStore(RegistryStoreAccessSettings settings) : base(settings) { }
+
+        public override void Load()
+        {
+            using (var rklm = RegistryKey.OpenBaseKey(AccessSettings.BaseKey, registryView))
+            {
+                var rk = rklm.OpenSubKey(AccessSettings.Path, true);
+                if (rk == null)
+                {
+                    Value = Activator.CreateInstance<T>();
+                    Save();
+                }
+                else
+                {
+                    using (rk)
+                    {
+                        var bytes = (byte[])rk.GetValue(AccessSettings.Key);
+                        LoadHandler(new MemoryStream(bytes));
+                    }
+                }
+            }
+        }
+
+        public override void Save()
+        {
+            SaveHandler(stream =>
+            {
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    stream.CopyTo(memoryStream);
+                    var bytes = memoryStream.ToArray();
+
+                    using (var rklm = RegistryKey.OpenBaseKey(AccessSettings.BaseKey, registryView))
+                    {
+                        var rk = rklm.OpenSubKey(AccessSettings.Path, true);
+                        if (rk == null)
+                        {
+                            rk = rklm.CreateSubKey(AccessSettings.Path);
+                        }
+
+                        using (rk)
+                        {
+                            rk.SetValue(AccessSettings.Key, bytes, RegistryValueKind.Binary);
+                        }
+                    }
+                }
+            });
+        }
+
+    }
+
+    /// <summary>
+    /// 本地存储器配置
+    /// </summary>
+    public class RegistryStoreAccessSettings : AccessSettings
+    {
+        public RegistryHive BaseKey { get; private set; }
+        public string Path { get; private set; }
+        public string Key { get; private set; }
+
+        public RegistryStoreAccessSettings(RegistryHive baseKey, string path, string key)
+        {
+            BaseKey = baseKey;
+            Path = path;
+            Key = key;
+        }
+    }
+}
---
+++ ApeFree.DataStore.Registry/RegistryStoreFactoryExtensions.cs
@@ -0,0 +1,20 @@
+﻿using ApeFree.DataStore.Registry;
+using Microsoft.Win32;
+
+namespace ApeFree.DataStore
+{
+    public static class RegistryStoreFactoryExtensions
+    {
+        /// <summary>
+        /// 创建一个注册表(对象)储存器
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="_"></param>
+        /// <param name="settings"></param>
+        /// <returns></returns>
+        public static RegistryStore<T> CreateRegistryStore<T>(this StoreFactory _, RegistryStoreAccessSettings settings) where T : new()
+        {
+            return new RegistryStore<T>(settings);
+        }
+    }
+}

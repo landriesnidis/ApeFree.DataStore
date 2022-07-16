4895c4048f91ead68d21bf83dd081f90ed7786ee
wowoi <1602397209@qq.com>
2022-07-17 01:06:53 +0800

    新增对Store的扩展方法：异步加载和异步保存

---
+++ ApeFree.DataStore/Extensions/StoreExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApeFree.DataStore.Core
+{
+    public static class StoreExtension
+    {
+        public static Task LoadAsync(this IStore store)
+        {
+            return Task.Run(() => store.Load());
+        }
+
+        public static Task SaveAsync(this IStore store)
+        {
+            return Task.Run(() => store.Save());
+        }
+    }
+}

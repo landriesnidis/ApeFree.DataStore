2a74d2fa23ab1375659093b0e4182c9c26c51b9c
Guijie Lee <landriesnidis@yeah.net>
2022-07-17 23:50:24 +0800

    新增Store专用的复用线程池管理器

---
+++ ApeFree.DataStore/Core/ReusableThreadPoolManager.cs
@@ -0,0 +1,25 @@
+﻿using STTech.CodePlus.Threading;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApeFree.DataStore.Core
+{
+    /// <summary>
+    /// 复用线程池管理器
+    /// </summary>
+    internal class ReusableThreadPoolManager
+    {
+        /// <summary>
+        /// 加了Lazy之后，只有当使用到的时候才加载
+        /// </summary>
+        private static readonly Lazy<ReusableThreadPoolManager> single = new Lazy<ReusableThreadPoolManager>(() => new ReusableThreadPoolManager());
+        public static ReusableThreadPoolManager Instance => single.Value;
+
+        public ReusableThreadPool Pool { get; }
+        private ReusableThreadPoolManager() {
+            Pool = new ReusableThreadPool();
+        }
+
+    }
+}

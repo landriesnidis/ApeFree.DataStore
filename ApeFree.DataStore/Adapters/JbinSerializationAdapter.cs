3ef68a05f2380209519c92b56822039d65ea42a6
Guijie Lee <landriesnidis@yeah.net>
2024-12-09 10:52:32 +0800

    新增Jbin序列化的实现

--- ApeFree.DataStore/Adapters/JbinSerializationAdapter.cs
+++ ApeFree.DataStore/Adapters/JbinSerializationAdapter.cs
@@ -1,4 +1,5 @@
 ﻿using ApeFree.DataStore.Core;
+using ApeFree.Protocols.Json.Jbin;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using System;
@@ -9,12 +10,12 @@ using System.Text;
 namespace ApeFree.DataStore.Adapters
 {
     /// <summary>
-    /// Json序列化器
+    /// Jbin序列化器
     /// </summary>
-    public class JsonSerializationAdapter : BaseSerializationAdapter
+    public class JbinSerializationAdapter : BaseSerializationAdapter
     {
         /// <summary>
-        /// 默认的Json序列化配置
+        /// 默认的Jbin序列化配置
         /// 私有静态，此对象用作全局的默认配置，不允许被外部获取，以免被修改
         /// </summary>
         protected readonly static Lazy<JsonSerializerSettings> serializerSettings = new Lazy<JsonSerializerSettings>(() =>
@@ -29,7 +30,7 @@ namespace ApeFree.DataStore.Adapters
         });
 
         /// <summary>
-        /// Json序列化配置
+        /// Jbin序列化配置
         /// </summary>
         public JsonSerializerSettings SerializerSettings { get; }
 
@@ -39,10 +40,10 @@ namespace ApeFree.DataStore.Adapters
         public Encoding Encoding { get; set; } = Encoding.UTF8;
 
         /// <summary>
-        /// 构造Json格式的序列化适配器
+        /// 构造Jbin格式的序列化适配器
         /// </summary>
         /// <param name="serializerSettings"></param>
-        public JsonSerializationAdapter(JsonSerializerSettings serializerSettings = null)
+        public JbinSerializationAdapter(JsonSerializerSettings serializerSettings = null)
         {
             SerializerSettings = serializerSettings;
         }
@@ -50,16 +51,17 @@ namespace ApeFree.DataStore.Adapters
         /// <inheritdoc/>
         public override T Deserialize<T>(Stream stream)
         {
-            StreamReader reader = new StreamReader(stream, Encoding);
-            var json = reader.ReadToEnd();
-            return JsonConvert.DeserializeObject<T>(json, SerializerSettings ?? serializerSettings.Value);
+            var jo = JbinObject.Parse(stream);
+            var obj = jo.ToObject<T>(SerializerSettings ?? serializerSettings.Value);
+
+            return obj;
         }
 
         /// <inheritdoc/>
         public override Stream Serialize(object obj)
         {
-            var json = JsonConvert.SerializeObject(obj, Formatting.Indented, SerializerSettings ?? serializerSettings.Value);
-            var bytes = Encoding.GetBytes(json);
+            var jo = JbinObject.FromObject(obj, SerializerSettings ?? serializerSettings.Value);
+            var bytes = jo.ToBytes();
             return new MemoryStream(bytes);
         }
 
